11ef44f00181987e63038cb81ac1f8ce15026e24
Lokesh hans <Lokeshbaghelhans@gmail.com>
2026-04-03 11:46:13 +0530

    fix scroll and motion

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,28 @@ import Login from "./pages/Login";
 import Webinars from "./pages/Webinars";
 import Blogs from "./pages/Blogs";
 import Rankings from "./pages/Rankings";
-
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      {/* Layout Wrapper */}
-      <Route path="/" element={<Layout />}>
-
-        <Route index  element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/highlights" element={<Highlights />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/podcasts" element={<Podcasts />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/rankings" element={<Rankings />} />
-        <Route path="/webinars" element={<Webinars />} />
-        <Route path="/login" element={<Login/>}> </Route>
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* Layout Wrapper */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/highlights" element={<Highlights />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/podcasts" element={<Podcasts />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/rankings" element={<Rankings />} />
+          <Route path="/webinars" element={<Webinars />} />
+          <Route path="/login" element={<Login />}>
+          </Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
     <section className="min-h-[550px] max-h-screen flex items-center bg-gradient-to-l   from-blue-900 to-blue-700 text-white px-6 md:px-16">
-      
       {/* Container */}
       <div className="grid md:grid-cols-2 gap-10 items-center w-full max-w-7xl mx-auto">
-        
         {/* LEFT SIDE */}
         <div>
-          <h1 className="text-4xl md:text-5xl font-bold leading-tight mb-6">
+          <motion.h1
+            className="text-4xl md:text-5xl font-bold leading-tight mb-6"
+            whileHover={{ scale: 1 }}
+            animate={{  transition: { duration: 2, ease: "linear" }, scale: 1.1 }}
+          >
             Empowering Future Legal Professionals
-          </h1>
+          </motion.h1>
 
           <p className="text-lg text-gray-200 mb-8 max-w-lg">
             Participate in competitions, internships, and track your growth.
@@ -19,11 +22,14 @@ export default function Hero() {
 
           {/* Buttons */}
           <div className="flex flex-wrap gap-4">
-            
             <Link to="/blogs">
-              <button className="bg-yellow-400 text-black px-6 py-3 rounded-lg font-semibold hover:scale-105 transition">
+              <motion.button className="bg-yellow-400 text-black px-6 py-3 rounded-lg font-semibold hover:scale-105 transition"
+              initial={{opacity:0}}
+              whileInView={{opacity:1}}
+              transition={{duration:1}}
+              >
                 Explore Programs
-              </button>
+              </motion.button>
             </Link>
 
             <Link to="/rankings">
@@ -31,7 +37,6 @@ export default function Hero() {
                 View Rankings
               </button>
             </Link>
-
           </div>
         </div>
 
@@ -42,9 +47,9 @@ export default function Hero() {
             loading="eager"
             alt="Legal Illustration"
             className="w-full max-md  rounded-lg shadow-lg"
+            
           />
         </div>
-
       </div>
     </section>
   );
